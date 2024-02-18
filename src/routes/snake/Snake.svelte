2ffad646cb492f65d62f6bb399d5fc84216fce3e
Patrick Hoke <pahoke@gmail.com>
2024-02-18 12:56:57 -0800

    add food generation and consumption

--- src/routes/snake/Snake.svelte
+++ src/routes/snake/Snake.svelte
@@ -6,12 +6,16 @@
 	const { width } = $props<Props>();
 
 	interface Cell {
-		on: boolean;
+		hasSnakeSegment: boolean;
+		hasFood?: boolean;
 	}
 
-	interface SnakeSegment {
+	interface Coordinate {
 		x: number;
 		y: number;
+	}
+
+	interface SnakeSegment extends Coordinate {
 		next?: SnakeSegment;
 	}
 
@@ -25,21 +29,33 @@
 		y: 0
 	});
 
+	function generateFood() {
+		return {
+			x: Math.floor(Math.random() * width),
+			y: Math.floor(Math.random() * width)
+		};
+	}
+
+	let food: Coordinate = $state(generateFood());
+
 	let grid: Cell[][] = $derived.by(() => {
 		let newGrid: Cell[][] = Array(width);
 		for (let i = 0; i < newGrid.length; i++) {
 			newGrid[i] = Array(width);
 			for (let j = 0; j < newGrid[i].length; j++) {
-				newGrid[i][j] = { on: false };
+				newGrid[i][j] = { hasSnakeSegment: false };
 			}
 		}
 		// let newGrid = Array<Cell[]>(width).fill(Array<Cell>(width).fill({ on: false }));
 		// paint the snake
 		let current: SnakeSegment | undefined = head;
 		do {
-			newGrid[current.y][current.x].on = true;
+			newGrid[current.y][current.x].hasSnakeSegment = true;
 			current = current.next;
 		} while (current);
+
+		newGrid[food.y][food.x].hasFood = true;
+
 		return newGrid;
 	});
 
@@ -79,20 +95,29 @@
 		};
 		head = newHead;
 
-		let current = head;
-		while (current?.next?.next) {
-			current = current.next;
+		if (head.x == food.x && head.y == food.y) {
+			food = generateFood();
+		} else {
+			let current = head;
+			while (current?.next?.next) {
+				current = current.next;
+			}
+			current.next = undefined;
 		}
-		current.next = undefined;
+
 		console.log('nextFrame');
 	}
-	// clearInterval(clear)
+
 	$effect(() => {
-		console.log('$effect');
-		let nextFrameInterval = setInterval(nextFrame, 500);
-		return () => clearTimeout(nextFrameInterval);
+		const nextFrameInterval = setInterval(nextFrame, 200);
+		return () => clearInterval(nextFrameInterval);
 	});
-	// $inspect(grid);
+
+	function getCellBackgroundColor(cell: Cell) {
+		if (cell.hasFood) return '#0F0';
+		if (cell.hasSnakeSegment) return '#000';
+		return '#FFF';
+	}
 </script>
 
 <h1>Snake</h1>
@@ -100,7 +125,7 @@
 {#each grid as row}
 	<div class="row">
 		{#each row as cell}
-			<div class="cell" style="--bg-color: {cell.on ? '#000' : '#FFF'}" />
+			<div class="cell" style="--bg-color: {getCellBackgroundColor(cell)}" />
 		{/each}
 	</div>
 {/each}
